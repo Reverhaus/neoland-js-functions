3d571019ef53d5a81f66c52b28703509486b4c2b
Reverhaus <javierbailengomez@gmail.com>
2024-02-12 17:01:22 +0100

    Iteration#1

---
+++ iteraction1.js
@@ -0,0 +1,10 @@
+console.log(`#Iteracción 1`)
+
+function sum(numberOne , numberTwo) {
+    // insert code
+    if(numberOne > numberTwo){
+        return numberOne;}
+        else {return numberTwo}
+    }
+
+    console.log(`el resutlado es ${sum(3,7)}`)
